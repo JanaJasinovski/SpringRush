e818db97c7ca4dc32fa7ca7dfbe2048726b37481
Yana Yasinvski <jantschick3@gmail.com>
2023-11-14 11:59:26 +0100

    Initial commit

---
+++ src/main/java/com/example/springRush/SpringRushApplication.java
@@ -0,0 +1,13 @@
+package com.example.springRush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springRush/SpringRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
