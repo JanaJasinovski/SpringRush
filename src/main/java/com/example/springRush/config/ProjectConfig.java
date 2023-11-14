3a8c7016d9406ab05bab2c9bf1d9303410677d4c
Yana Yasinvski <jantschick3@gmail.com>
2023-11-14 12:43:11 +0100

    Spring Context

---
+++ src/main/java/com/example/springRush/config/ProjectConfig.java
@@ -0,0 +1,44 @@
+package com.example.springRush.config;
+
+import com.example.springRush.model.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+//@ComponentScan("com.example.springRush.model")
+public class ProjectConfig {
+
+//    @Bean
+//    Parrot parrot1() {var p = new Parrot();
+//        p.setName("Koko");
+//        return p;
+//    }
+
+//    @Bean(name = "miki")
+//    @Bean(value = "miki")
+//    @Bean("miki")
+//    @Bean
+//    @Primary
+//    Parrot parrot2() {
+//        var p = new Parrot();
+//        p.setName("Miki");
+//        return p;
+//    }
+//    @Bean
+//    Parrot parrot3() {
+//        var p = new Parrot();
+//        p.setName("Riki");
+//        return p;
+//    }
+
+//    @Bean
+//    String hello() {
+//        return "Hello";
+//    }
+//    @Bean
+//    Integer ten() {
+//        return 10;
+//    }
+}
